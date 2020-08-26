237e137ae529bf5b633ddd8f35209a3a4ef72288
Rubem Fridolio Christ Neto <56802722+RubemNto@users.noreply.github.com>
2020-08-25 21:08:31 -0300

    Add files via upload

---
+++ levelselection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelselection : MonoBehaviour
+{
+    [Header ("levels of the game")]
+    public string[] levelNames;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadLevel()
+    {
+        string name =  EventSystem.current.currentSelectedGameObject.name;
+        for(int i = 0; i < levelNames.Length; i++)
+        {
+            if(name == levelNames[i])
+            {
+                Debug.Log("play level" + name);
+                SceneManager.LoadScene(name);
+            }
+        }
+    }
+}
